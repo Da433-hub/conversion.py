c2e2a39094bdfa82fa63b9b48246bffa3baa0556
Daniel Carty <dan120.c1e@gmail.com>
2024-01-22 00:12:07 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,12 @@
+# Create the given variables in Practical Task 3.
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert each variable seperately.
+print (int(num1))
+print (float(num2))
+print (str(num3))
+print (int(string1))
